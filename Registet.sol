6ee06b2d17a7e0b95f4561aa0449d187d62e86c4
gansabo <108462519+gansabo@users.noreply.github.com>
2023-06-24 11:38:49 +0700

    Register.sol

--- Registet.sol
+++ Registet.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Register {
+    string public github;
+    address public owner;
+    
+    struct Referral {
+        address referralAddress;
+        string referralString;
+    }
+    
+    Referral[] public referrals;
+    
+    constructor() {
+        github = "gansabo";
+        owner = 0x57EbE22673ea62d1B9BAf556b16c9476Fd0A7A68;
+    }
+    
+    function addReferral(address _referralAddress, string memory _referralString) external {
+        require(msg.sender == owner, "Only the owner can add referrals.");
+        referrals.push(Referral(_referralAddress, _referralString));
+    }
+    
+    function totalReferrals() public view returns (uint256) {
+        return referrals.length;
+    }
+}
